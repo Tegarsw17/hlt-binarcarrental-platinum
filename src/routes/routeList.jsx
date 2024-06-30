c94bf57af8a0c85555b1af02f3ccb64394e6bf46
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2024-07-01 00:52:49 +0700

    feat: add route for login and register

--- src/routes/routeList.jsx
+++ src/routes/routeList.jsx
@@ -3,11 +3,21 @@ import Home from '../pages/customer/Home';
 import Search from '../pages/customer/Search';
 import Car from '../pages/customer/Car';
 import CarDetail from '../pages/customer/CarDetail';
+import Login from '../pages/customer/Login';
+import Register from '../pages/customer/Register';
 
 export const routeList = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
   },
   {
     path: '/search',
