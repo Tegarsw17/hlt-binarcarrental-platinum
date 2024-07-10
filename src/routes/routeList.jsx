33c4ed335189913acf67362dd9347013f62f8c47
Tegarsw17 <tegarsatriyawiguna99@gmail.com>
2024-07-10 14:30:56 +0700

    fix: route conflict

--- src/routes/routeList.jsx
+++ src/routes/routeList.jsx
@@ -3,15 +3,10 @@ import Home from '../pages/customer/Home';
 import Search from '../pages/customer/Search';
 import Car from '../pages/customer/Car';
 import CarDetail from '../pages/customer/CarDetail';
-<<<<<<< HEAD
 import AdminListCar from '../pages/admin/ListCar/index';
-=======
 import Payment from '../pages/customer/Payment';
 import Login from '../pages/customer/Login';
 import Register from '../pages/customer/Register';
-
-
->>>>>>> 44916e54eeae22c2556a16efa83ed39898d9d5c4
 export const routeList = createBrowserRouter([
   {
     path: '/',
@@ -38,12 +33,11 @@ export const routeList = createBrowserRouter([
     element: <CarDetail />,
   },
   {
-<<<<<<< HEAD
-    path: '/admin/listcar',
-    element: <AdminListCar />,
-=======
     path: '/payment/:id',
     element: <Payment />,
->>>>>>> 44916e54eeae22c2556a16efa83ed39898d9d5c4
+  },
+  {
+    path: '/admin/listcar',
+    element: <AdminListCar />,
   },
 ]);
